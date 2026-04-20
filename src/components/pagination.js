7c32797e16bdacb6c1ababf245b1e47cc964a2fd
Pchola <zhosan.e-1@yandex.ru>
2026-04-20 20:34:14 +0300

    подготовил 7 проект к сдаче 2.0

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -37,8 +37,8 @@ export const initPagination = ({pages, fromRow, toRow, totalRows}, createPage) =
         }));
 
         fromRow.textContent = (page - 1) * limit + 1; // С какой строки выводим
-        toRow.textContent = Math.min((page * limit), pageCount); // До какой строки выводим, если это последняя страница, то отображаем оставшееся количество
-        totalRows.textContent = pageCount; // Сколько всего строк выводим на всех страницах вместе (после фильтрации будет меньше) 
+        toRow.textContent = Math.min((page * limit), total); // До какой строки выводим, если это последняя страница, то отображаем оставшееся количество
+        totalRows.textContent = total; // Сколько всего строк выводим на всех страницах вместе (после фильтрации будет меньше) 
     }
     
     return {
